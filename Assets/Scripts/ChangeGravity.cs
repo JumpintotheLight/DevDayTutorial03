af6af7328e3ccb20eb27e6abf0e5d92d2c8fc99f
Sky Rolnick <skymeson@gmail.com>
2017-06-26 18:31:18 -0400

    Dev Day Tutorial 03
    Gravity Blocks, a simple game demonstrating the use UI elements,
    scripting, and physics.gravity.

---
+++ Assets/Scripts/ChangeGravity.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeGravity : MonoBehaviour
+{
+
+    public float gravity_scale;
+
+    void Start()
+    {
+        Physics.gravity = new Vector3(0, gravity_scale, 0);
+    }
+
+
+
+    void Update()
+    {
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * gravity_scale * 1000.0f;
+        var z = Input.GetAxis("Vertical") * Time.deltaTime * gravity_scale * 1000.0f;
+
+        //transform.Rotate(0, x, 0);
+        //transform.Translate(0, 0, z);
+
+        Physics.gravity = new Vector3(x, -1.0f, z);
+        Debug.Log("Physics.gravity.magnitude." + Physics.gravity.magnitude);
+    }
+}
---
+++ Assets/Scripts/InstantiateCube.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class InstantiateCube : MonoBehaviour
+{
+    public GameObject prefab;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        for (int i = 0; i < 10; i++)
+        {
+            for (int j = 0; j < 10; j++)
+            {
+                Instantiate(prefab, new Vector3(10.0f * j, 10 * i + 10.0f, 0.0f), Quaternion.identity);
+            }
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
